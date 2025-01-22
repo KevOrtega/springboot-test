d48fee27f9db27fe65f162369781a0a93b2ce15f
KevOrtega <kevortega.dev@gmail.com>
2025-01-22 16:36:18 -0300

    feat: create ExampleRestController - use RestController annotation to class

---
+++ src/main/java/com/kevortega/spring/springboot_test/controllers/ExampleRestController.java
@@ -0,0 +1,9 @@
+package com.kevortega.spring.springboot_test.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ExampleRestController {
+
+}
