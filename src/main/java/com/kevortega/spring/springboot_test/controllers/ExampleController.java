72bc4431868615f5a118f8246e507987d7cc0db5
KevOrtega <kevortega.dev@gmail.com>
2025-01-27 04:35:41 -0300

    feat: add employees table to [GET]/info_details page - use ModelAttribute to set list of employees - render employees on a new table in html

--- src/main/java/com/kevortega/spring/springboot_test/controllers/ExampleController.java
+++ src/main/java/com/kevortega/spring/springboot_test/controllers/ExampleController.java
@@ -1,8 +1,12 @@
 package com.kevortega.spring.springboot_test.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.kevortega.spring.springboot_test.models.Employee;
 
@@ -23,8 +27,19 @@ public class ExampleController {
 
     @GetMapping("/info_details")
     public String info(Model model){
-        Employee employee = new Employee(1, 35, 123456789, "Kevin", "Ortega", "123 Main Street", "Developer");
+        Employee employee = new Employee(1, 35, 123456789, "Kevin", "Ortega", null, "Developer");
         model.addAttribute("employee", employee);
         return "info_details";
     }
+
+
+@ModelAttribute("Employees")
+public List<Employee> employees(){
+    return Arrays.asList(
+        new Employee(1, 35, 123456789, "Kevin", "Ortega", null, "Developer"),
+        new Employee(2, 30, 987654321, "Juan", "Perez", null, "QA"),
+        new Employee(3, 25, 555555555, "Pedro", "Garcia", null, "DevOps")
+    );
+}
+
 }
