aff08731dd9411d3d6954db4bdf4c843601623f1
KevOrtega <kevortega.dev@gmail.com>
2025-01-27 15:48:24 -0300

    feat: [POST]/api/variable/createEmployee endpoint added

--- src/main/java/com/kevortega/spring/springboot_test/controllers/PathVariableController.java
+++ src/main/java/com/kevortega/spring/springboot_test/controllers/PathVariableController.java
@@ -2,8 +2,12 @@ package com.kevortega.spring.springboot_test.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kevortega.spring.springboot_test.models.Employee;
 
 @RestController
 @RequestMapping("api/variable")
@@ -14,4 +18,10 @@ public class PathVariableController {
         paramsDTO.setInformation(message);
         return paramsDTO;
     }
+
+    @PostMapping("/createEmployee")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employee;
+    }
+
 }
