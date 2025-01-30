f867ab8ce32290f2706361a2a84dbb1fabc7790b
KevOrtega <kevortega.dev@gmail.com>
2025-01-30 09:35:36 -0300

    feat: add [GET]/api/variable/values endpoint - values controller created and returns data - got config properties to send to user

--- src/main/java/com/kevortega/spring/springboot_test/controllers/PathVariableController.java
+++ src/main/java/com/kevortega/spring/springboot_test/controllers/PathVariableController.java
@@ -1,5 +1,9 @@
 package com.kevortega.spring.springboot_test.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +16,19 @@ import com.kevortega.spring.springboot_test.models.Employee;
 @RestController
 @RequestMapping("api/variable")
 public class PathVariableController {
+
+    @Value("${config.user}")
+    private String user;
+
+    @Value("${config.code}")
+    private int code;
+
+    @Value("${config.message}")
+    private String message;
+
+    @Value("${config.values}")
+    private String[] values;
+
     @GetMapping("/page1/{message}")
     public ParamsDTO pagina1(@PathVariable String message) {
         ParamsDTO paramsDTO = new ParamsDTO();
@@ -22,6 +39,17 @@ public class PathVariableController {
     @PostMapping("/createEmployee")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employee;
+    }
+
+    
+    @GetMapping("/values")
+    public Map<String, Object> getValues() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("user", user);
+        map.put("code", code);
+        map.put("message", message);
+        map.put("values", values);
+        return map;
     }
 
 }
